211a4a895f19a832db582ef5a64da11ffc1895eb
root <root@localhost>
2021-06-04 03:47:14 +0000

    finalised the address functionalaty (for now)

--- server.js
+++ server.js
@@ -8,8 +8,8 @@ const myChain = new Blockchain();
 let port = process.env.PORT || 3000
 let blockchain;
 let activeminers = [];
-let testpub = 'FORDYxa16d78a471e0dc4f2cdfec6fdbebde5985ce907dc35c89eee194c6713e24d57c';
-let testpriv = '8f7cedcdb0bc98e3b82711bfe25c47f4cd6755f7227f9f264772f9278d19cb67' ;
+let testpub = 'FORDYxff3baf17698aa6ee86002e22450a8fcc4acda4c4829d37437b23e996b88b7b88';
+let testpriv = 'd2867e2e46030fa4570baa8c7674b41784fd1dbc0a76e5c192054d5cef36101d100d4734ee05b78c74bb3604ca834dbb39c190bcc75ce12ee523135de99edd10';
 
 const server = http.createServer(function(req, res) {
   let parsedURL = url.parse(req.url, true);
@@ -38,6 +38,7 @@ const server = http.createServer(function(req, res) {
   });
 });
 
+//FORDYxae250f0d6d54616c3091c8b6b1c6d8d1cc1f1b7735437a463b3a9de4cedfe77e
 
 function gotdata(data) {
   blockchain = JSON.parse(data);
@@ -45,7 +46,9 @@ function gotdata(data) {
   myChain.difficulty = blockchain.difficulty;
   myChain.pendingTransactions = blockchain.pendingTransactions;
   myChain.miningReward = blockchain.miningReward;
- // maketx();
+/*  let tmp = myChain.getNewminingblock();
+  myChain.addblock(tmp,testpub);
+*/
 }
 
   fs.readFile("fordycoin-blockchain.json", (err, data) => gotdata(data));
@@ -61,33 +64,31 @@ function fixstring(str) {
    return str;
 }
 
-function newblock(address) {
-      let data = myChain.getNewminingblock();
-      let miner = {"hash":data.hash,"miner":address};
-      activeminers.push(miner);
-      data = JSON.stringify(data);
-      return data; 
-}
+function sendTransaction(fromAddress, toAddress, amount, priv) {
+   let tmp3 = parseInt(amount);
+   if ((myChain.getBalanceOfAddress(fromAddress) - tmp3)  < 0) {
+       return 'fromAddress does not have enough funds';
+   }
+   let tmp = new Transaction(fromAddress, toAddress, amount);
+   let tmp2 = tmp.signTransaction(priv);
+   if(!tmp2 == 'success') {
+      return tmp2;
+   }
+  tmp2 = myChain.addTransaction(tmp,priv);
+   if(!tmp2 == 'success') {
+      return tmp2;
+   }
+    console.log(myChain.pendingTransactions);
+  return 'success';
 
-function addblock(block) {
-     let mhash = block.hash;
-     for (let i=0; i<activeminers.length;i++) {
-       if (mhash==activeminers[i].hash) {
-           let address = activeminers[i].miner;
-           myChain.addblock(block, address);
-           return 'succses';
-       }
-    }
-  return 'are you sure thats a valaid block?';
 }
-
 
 let routes = {
-    "mine/newblock": function(data, res) {
+    "main/createAddress": function(data, res) {
       let string = data.queryString;
       string = JSON.stringify(string);
       string = fixstring(string);
-      let payload = newblock(string);
+      let payload = myChain.createAddress(string);
       let payloadStr = JSON.stringify(payload);
       res.setHeader("Content-Type", "application/json");
       res.setHeader("Access-Control-Allow-Origin", "*");
@@ -95,12 +96,22 @@ let routes = {
       res.write(payloadStr);
       res.end("\n");
     },
-   "mine/addblock": function(data, res) {
 
+    // http://localhost:3000/main/sendTransaction?data='FORDYxff3baf17698aa6ee86002e22450a8fcc4acda4c4829d37437b23e996b88b7b88,FORDYxae250f0d6d54616c3091c8b6b1c6d8d1cc1f1b7735437a463b3a9de4cedfe77e,2,d2867e2e46030fa4570baa8c7674b41784fd1dbc0a76e5c192054d5cef36101d100d4734ee05b78c74bb3604ca834dbb39c190bcc75ce12ee523135de99edd10'
+
+    "main/sendTransaction": function(data, res) {
+      let string = data.queryString;
+      string = JSON.stringify(string);
+      string = fixstring(string);
+      string = string.split(',');
+      let payload = sendTransaction(string[0], string[1], string[2], string[3]);
+      let payloadStr = JSON.stringify(payload);
+      res.setHeader("Content-Type", "application/json");
+      res.setHeader("Access-Control-Allow-Origin", "*");
+      res.writeHead(200);
+      res.write(payloadStr);
+      res.end("\n");
     },
-  "all": function(data, res) {
- 
-  },
   notFound: function(data, res) {
     let payload = {
       message: "File Not Found",
